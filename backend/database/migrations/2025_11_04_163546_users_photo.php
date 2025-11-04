15ea9b23198ac5122759086dc78b67a070675e2e
Żenia <zenasamarash@gmail.com>
2025-11-04 17:44:57 +0100

    Migrations for users photo.

---
+++ backend/database/migrations/2025_11_04_163546_users_photo.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('photo_url', 255);
+        });
+
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->string('photo_url', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('photo_url');
+        });
+
+        Schema::dropIfExists('photos');
+    }
+};
